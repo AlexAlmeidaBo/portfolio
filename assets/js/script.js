897c5b4a9c11791dde14bed58d27e2d34dbae68b
Alex Almeida <almeidaa.dev@gmail.com>
2023-04-29 04:01:11 -0300

    adição da versão mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,37 @@
-const titulo = document.querySelector('.nome');
-
+/*const titulo = document.querySelector('.nome');
 function typeWrite(elemento){
     const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-
-
+    elemento.innerHTML = '';   
     textoArray.forEach( (letra, i) => {
         setTimeout(function(){
             elemento.innerHTML += letra; 
         }, 125 * i)
     });
 }
-typeWrite(titulo)
+typeWrite(titulo)*/
+
+const typewriter = document.querySelector(".nome");
+const text = "Alex Almeida";
+
+let i = 0;
+let isDeleting = false;
+
+function type() {
+  const speed = Math.random() * 200 + 50;
+
+  if (!isDeleting && i < text.length) {
+    typewriter.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, speed);
+
+  } else if (isDeleting && i >= 0) {
+    typewriter.innerHTML = text.substring(0, i);
+    i--;
+    setTimeout(type, speed);
+
+  } else {
+    isDeleting = !isDeleting;
+    setTimeout(type, 1500);
+  }
+}
+type()
---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.conteudo-escrito',{delay:200})
+ScrollReveal().reveal('.container-projetos',{delay:200})
+ScrollReveal().reveal('.container-contato',{delay:200})
